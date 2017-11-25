d7ac67b19e30c818eab214e8096229e541f51263
Jonny Lu <zlu883@aucklanduni.ac.nz>
2017-11-26 11:27:17 +1300

    Added basic bot framework connection

---
+++ app.js
@@ -0,0 +1,24 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: "c380f179-1f19-4c2d-879c-8ad2d0c8339d",
+    appPassword: "lxvuXY128ypaPMJYN50?=@?"
+    //appId: process.env.MICROSOFT_APP_ID,
+    //appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
