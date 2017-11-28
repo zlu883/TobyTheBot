b3e44596e148b0817317e36498a2243274c53847
Jonny Lu <zlu883@aucklanduni.ac.nz>
2017-11-28 22:56:31 +1300

    changed to use bot framework appid

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    //appId: "c380f179-1f19-4c2d-879c-8ad2d0c8339d",
-    //appPassword: "lxvuXY128ypaPMJYN50?=@?"
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "c380f179-1f19-4c2d-879c-8ad2d0c8339d",
+    appPassword: "lxvuXY128ypaPMJYN50?=@?"
+    //appId: process.env.MICROSOFT_APP_ID,
+    //appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users 
